f4dbf52a86c7ef78e94324ec4ac31ef0289f1e8d
Buppi Khan <khanbuppi@gmail.com>
2022-06-24 19:23:57 -0400

    added getters, setters, functions on wallet

---
+++ Day 11/Integers.java
@@ -0,0 +1,5 @@
+public class Integers {
+    public static void main(String[] args) {
+        
+    }    
+}
---
+++ Day 8/Day/wallet.java
@@ -0,0 +1,90 @@
+
+public class wallet {
+    private int penny, nickel, dime, quarter;
+    private int dollar, five, ten, twenty, fifty, oneHundred;
+    public static void main(int[] args) {
+
+    }
+    //default constructor
+    public wallet() {
+
+    }
+
+    //addMoney function
+    public void addMoney() {
+
+    }
+    //spendMoney function
+    public double spendMoney() {
+        return spendMoney();
+    }
+
+
+
+    //setters and getters
+    public int getPenny() {
+        return penny;
+    }
+    public void setPenny(int penny) {
+        this.penny = penny;
+    }
+    public int getNickel() {
+        return nickel;
+    }
+    public void setNickel(int nickel) {
+        this.nickel = nickel;
+    }
+    public int getDime() {
+        return dime;
+    }
+    public void setDime(int dime) {
+        this.dime = dime;
+    }
+    public int getQuater() {
+        return quarter;
+    }
+    public void setQuarter(int quarter) {
+        this.quarter = quarter;
+    }
+    public int getDollar() {
+        return dollar;
+    }
+    public void setDollar(int dollar) {
+        this.dollar = dollar;
+    }
+    public int getFive() {
+        return five;
+    }
+    public void setfive(int five) {
+        this.five = five;
+    }
+    public int getTen() {
+        return ten;
+    }
+    public void setTen(int ten) {
+        this.ten = ten;
+    }
+    public int getTwenty() {
+        return twenty;
+    }
+    public void setTwenty(int twenty) {
+        this.twenty = twenty;
+    }
+    public int getFifty() {
+        return fifty;
+    }
+    public void setFifty(int fifty) {
+        this.fifty = fifty;
+    }
+    public int getoneHundred() {
+        return oneHundred;
+    }
+    public void setgetoneHundred(int oneHundred) {
+        this.oneHundred = oneHundred;
+    }
+    /*
+    //toString
+    public String toString() {
+        
+    }*/
+}
