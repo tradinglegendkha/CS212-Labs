0f06ba7227c1d6c21eb47f5dfae042070e5081f4
Buppi Khan <khanbuppi@gmail.com>
2022-06-27 18:04:32 -0400

    able to run now

--- Day11/Integers.java
+++ Day11/Integers.java
@@ -1,21 +1,24 @@
 package Day11;
+import java.util.Arrays;
 
 public class Integers {
     public static void main(String[] args) {
-        int nums = [2,7,11,15];
-        int target = 9;
+        Integer[] nums = {2,7,11,15};
+        Integer[] nums2 = {3,2,4};
+        //System.out.println(twoSum(nums, 9));
+        System.out.println(Arrays.toString(twoSum(nums, 9)));
+        System.out.println(Arrays.toString(twoSum(nums2, 6)));
         
     }  
     
-    public int[] twoSum(int[] nums, int target) {
-        int n=nums.length;
-        for(int i=0; i <n-1; i++) {
-            for(j=i+1; j<n; j++) {
+    public static int[] twoSum(Integer[] nums, int target) {
+        for(int i=0; i <nums.length-1; i++) {
+            for(int j=i+1; j<nums.length; j++) {
                 if(nums[i]+nums[j]==target) {
-                    return new int[]{i,j};
+                    return new int[]{i, j};
                 }
             }
         }
-        return new int[] {-1,-1};
+        return new int[] {0, 0};
     }
 }
